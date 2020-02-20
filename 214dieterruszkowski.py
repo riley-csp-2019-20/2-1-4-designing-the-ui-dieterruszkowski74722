f61b2780e6b65970b4b4cb464417c50aba3b3386
dieterruszkowski74722 <55498038+dieterruszkowski74722@users.noreply.github.com>
2020-02-20 09:58:02 -0500

    Add files via upload

--- 214dieterruszkowski.py
+++ 214dieterruszkowski.py
@@ -5,15 +5,14 @@ root = tk.Tk()
 root.wm_geometry("200x200")
 root.title("authorization")
 
-frame_auth = tk.Frame(root)
-frame_auth.grid(row= 0,column= 0, sticky="news")
-
 frame_login = tk.Frame(root)
-frame_login.grid()
+frame_login.grid(row= 0,column= 0, sticky="news")
 
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack(padx=50)
 
+ent_username = tk.Entry(frame_login, bd=3)
+ent_username.pack()
 
 lbl_password = tk.Label(frame_login, text='Password:')
 lbl_password.pack(padx=50)
@@ -21,16 +20,23 @@ lbl_password.pack(padx=50)
 ent_password = tk.Entry(frame_login, bd=3, show='*')
 ent_password.pack()
 
-ent_username = tk.Entry(frame_login, bd=3)
-ent_username.pack()
 
+def test_button():
+    print("click")
+    frame_auth.tkraise()
+    password = ent_password.get()
+    lbl_display_pass.config(text = password)
 
-button_login = tk.Button(frame_login, text="Login")
+button_login = tk.Button(frame_login, text="Login", command=test_button)
 button_login.pack()
 
 
 frame_auth = tk.Frame(root)
 frame_auth.grid(row= 0,column= 0, sticky="news")
 
+lbl_display_pass = tk.Label(frame_auth, text = "test")
+lbl_display_pass.pack()
+
+frame_login.tkraise()
 
 root.mainloop()
